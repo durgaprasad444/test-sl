4c5a346f45dcc24048fd2728eae91fbcda1a2ab6
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-07-10 18:07:35 +0530

    Update stdPipeline.groovy

--- src/org/acme/stdPipeline.groovy
+++ src/org/acme/stdPipeline.groovy
@@ -8,7 +8,7 @@ def execute() {
     stage('Initialize') {
       checkout scm
       echo 'Loading pipeline definition'
-      Yaml parser = new Yaml()
+      yaml parser = new yaml()
       Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yaml').text)
     }
 
